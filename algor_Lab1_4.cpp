fced43d19b82244ec599ebc06a98d4b1fb208c94
8ReTid8 <125667954+8ReTid8@users.noreply.github.com>
2023-12-23 19:21:10 +0700

    gay

--- algor_Lab1_4.cpp
+++ algor_Lab1_4.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    bool temp = true;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    bool minmax = true;
-    bool maxmin = true;
-    for(int i=1;i<n;i++){
-        if(arr[i-1]>arr[i]){
-            minmax = false;
-        }
-        if(arr[i-1]<arr[i]){
-            maxmin = false;
-        }   
-    }
 
-    if(minmax){
-        cout<<"yes";
+bool check(int arr[],int n,int i=1){
+    if(n==i){
+        return true;
     }
-    else if(maxmin){
-        cout<<"yes";
+    else if(arr[i-1]<=arr[i]){
+        return check(arr,n,i+1);
     }
     else{
-        cout<<"no";
+        return false;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    bool temp = true;
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    bool checksort = check(arr,n);
+    
+    if (checksort)
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "no";
     }
 }
