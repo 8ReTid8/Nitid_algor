1979e7a7ecca56b5e14d701d986a2403aad072cc
8ReTid8 <125667954+8ReTid8@users.noreply.github.com>
2024-01-08 12:21:03 +0700

    gay

--- algor_Lab1_2.cpp
+++ algor_Lab1_2.cpp
@@ -4,25 +4,40 @@ int main(){
     int n;
     cin>>n;
     int a[n];
+    unordered_map<int ,int> counting;
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
     int Max = 0;
     int Min = INT_MAX;
-
     for(int i=0;i<n;i++){
-        int count = 0;
-        for(int j=0;j<n;j++){
-            if(a[i]==a[j]){
-                count++;
-            }
+        counting[a[i]]++;
+        
+    }
+    for(int i=0;i<n;i++){
+
+        if(Max<counting[i]){
+            Max = counting[i];
         }
-        if(Max<count){
-            Max = count;
-        }
-        if(Min>count){
-            Min = count;
+        if(Min>counting[i]&&counting[i]!=0){
+            Min = counting[i];
         }
     }
+
+    // for(int i=0;i<n;i++){
+    //     int count = 0;
+    //     for(int j=0;j<n;j++){
+    //         if(a[i]==a[j]){
+    //             count++;
+    //         }
+    //     }
+    //     if(Max<count){
+    //         Max = count;
+    //     }
+    //     if(Min>count){
+    //         Min = count;
+    //     }
+    // }
+    cout<<Max<<" "<<Min<<endl;
     cout<<Max-Min;
 }
--- algor_Lab1_3.cpp
+++ algor_Lab1_3.cpp
@@ -4,36 +4,50 @@ int main(){
     int n;
     cin>>n;
     int a[n];
+    unordered_map<int ,int> counting;
     int b[n];
+
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
     for(int i=0;i<n;i++){
         cin>>b[i];
     }
+    int Time = 0;
     int Max = 0;
-    int Max_val = 0;
-   
 
     for(int i=0;i<n;i++){
-        int MaxA = 0;
-        int MaxB = 0;
-        for(int j=0;j<n;j++){
-            if(a[i]==a[j]||a[i]==b[j]){
-                MaxA++;
-            }
-            if(b[i]==b[j]||b[i]==a[j]){
-                MaxB++;
-            }
+        counting[a[i]]++;
+        counting[b[i]]++;
+        if(Max<counting[a[i]]){
+            Max = counting[a[i]];
+            Time = a[i];
         }
-        if(Max < MaxA){
-            Max = MaxA;
-            Max_val = a[i];
-        }
-        if(Max < MaxB){
-            Max = MaxB;
-            Max_val = b[i];
+        if(Max<counting[b[i]]){
+            Max = counting[b[i]];
+            Time = b[i];
         }
     }
-    cout<<Max_val<<" "<<Max<<endl;
+
+    // for(int i=0;i<n;i++){
+    //     int MaxA = 0;
+    //     int MaxB = 0;
+    //     for(int j=0;j<n;j++){
+    //         if(a[i]==a[j]||a[i]==b[j]){
+    //             MaxA++;
+    //         }
+    //         if(b[i]==b[j]||b[i]==a[j]){
+    //             MaxB++;
+    //         }
+    //     }
+    //     if(Max < MaxA){
+    //         Max = MaxA;
+    //         Max_val = a[i];
+    //     }
+    //     if(Max < MaxB){
+    //         Max = MaxB;
+    //         Max_val = b[i];
+    //     }
+    // }
+    cout<<Time<<" "<<Max<<endl;
 }
