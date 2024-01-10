8e983b8949ddb4ba3d77533ddac35eb02893de49
8ReTid8 <125667954+8ReTid8@users.noreply.github.com>
2024-01-10 14:23:47 +0700

    check

--- algor_work5_3.cpp
+++ algor_work5_3.cpp
@@ -1,17 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void UNcheck(int &n,int start,int U[],int value){
-    bool check = true;
-    for(int i=start;i<n;i++){
-        if(value == U[i]){
-            check = false;
+// void UNcheck(int &n,int start,int U[],int value){
+//     bool check = true;
+//     for(int i=start;i<n;i++){
+//         if(value == U[i]){
+//             check = false;
+//         }
+//     }
+//     if(check){
+//         U[n] = value;
+//         n++;
+//     }
+// }
+void counting(int a[],int b[],int n,int Intersect[],int Union[],int& un,int& in){
+    int m=0;
+    for(int i=0;i<n;i++){
+        m = max(m,a[i]);
+        m = max(m,b[i]);
+    }
+    vector<int> countingA(m+1,0);
+    vector<int> countingB(m+1,0);
+    for(int i=0;i<n;i++){
+        countingA[a[i]]++;
+        countingB[b[i]]++;
+    }
+
+    for(int i=0;i<n;i++){
+        if(countingA[a[i]]>0 && countingB[a[i]]>0){
+            Intersect[in] = a[i];
+            in++;
         }
     }
-    if(check){
-        U[n] = value;
-        n++;
+    for(int i=0;i<n;i++){
+        if(countingA[a[i]]>0){
+            Union[un] = a[i];
+            un++;
+            countingB[a[i]] = 0;
+        }   
     }
+    for(int i=0;i<n;i++){
+        if(countingB[b[i]]>0){
+            Union[un] = b[i];
+            un++;
+        } 
+    }
+    
 }
 
 int main(){
@@ -25,32 +59,32 @@ int main(){
     int countInter = 0;
     for(int i=0;i<n;i++){
         cin>>a[i];
-        UNcheck(countUn,0,Union,a[i]);
+        // UNcheck(countUn,0,Union,a[i]);
     }
     for(int i=0;i<n;i++){
         cin>>b[i];
     }
     
-    int oldcountUn = countUn;
-    for(int i=0;i<n;i++){
-        bool tempUn = true;
-        bool tempInter = false;
-        for(int j=0;j<n;j++){
-            if(b[i]==Union[j]){
-                tempUn = false;
-            }
-            if(a[i]==b[j]){
-                tempInter = true;
-            }
-        }
-        if(tempUn){
-            UNcheck(countUn,oldcountUn,Union,b[i]);
-        }
-        if(tempInter){
-            UNcheck(countInter,0,Intersect,a[i]);
-        }
-    }
-
+    // int oldcountUn = countUn;
+    // for(int i=0;i<n;i++){
+    //     bool tempUn = true;
+    //     bool tempInter = false;
+    //     for(int j=0;j<n;j++){
+    //         if(b[i]==Union[j]){
+    //             tempUn = false;
+    //         }
+    //         if(a[i]==b[j]){
+    //             tempInter = true;
+    //         }
+    //     }
+    //     if(tempUn){
+    //         UNcheck(countUn,oldcountUn,Union,b[i]);
+    //     }
+    //     if(tempInter){
+    //         UNcheck(countInter,0,Intersect,a[i]);
+    //     }
+    // }
+    counting(a,b,n,Intersect,Union,countUn,countInter);
     for(int i=0;i<countInter;i++){
         cout<<Intersect[i]<<" ";
     }
