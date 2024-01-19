85e02ff3c869dba0daf1cdfd0e7519f17660ff23
8ReTid8 <125667954+8ReTid8@users.noreply.github.com>
2024-01-19 17:44:54 +0700

    hoy

--- algor_lupon4_1.cpp
+++ algor_lupon4_1.cpp
@@ -1,13 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int binarysearch(int A[],int n,int m){
+    int l=0;
+    int r=n-1;
+    
+    while(l<r){
+        int mid = (l+r)/2;
+        cout<<mid<<endl;
+        if(m<A[mid]){
+            r = mid-1;
+        }
+        if(m>A[mid]){
+            l = mid+1; 
+        }
+        if(m==A[mid]){
+            return mid;
+        }
+    }
+    return l;
+}
+
 void findK(int A[], int M, int k, int n)
 {
     int i = 0;
-    while ((A[i] < M) && (i < n)){
-        i++;
-    }
-
+    // while ((A[i] < M) && (i < n)){
+    //     i++;
+    // }
+    i = binarysearch(A,n,M);
     int left = i - 1; // set left, right
     int right = i;
 
@@ -29,7 +49,7 @@ void findK(int A[], int M, int k, int n)
 int main()
 {
     int A[] = {10, 12, 15, 17, 18, 20, 25};
-    int M = 11, k = 8;
+    int M = 16, k = 2;
     int n = sizeof(A)/sizeof(A[0]);
     A[-1] = INT_MAX;
     A[n] = INT_MAX;
