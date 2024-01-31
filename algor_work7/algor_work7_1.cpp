64bd4a188aac4d6f639066f66dea2fb409bded92
8ReTid8 <125667954+8ReTid8@users.noreply.github.com>
2024-01-31 12:45:15 +0700

    o

---
+++ algor_work7/algor_work7_1.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n=3;
+    string arr = "ABC";
+    cout<<arr[0];
+    return 0;
+}
---
+++ algor_work7/algor_work7_5.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int check(int x[], int k,int a[])
+{
+    int sum = 0; 
+    for (int i = 0; i < k-1; i++)
+    {
+        if(a[i] && a[i+1]){
+            return 0;
+        }
+        if(a[i]==1){
+            sum+=x[i];
+        }
+    }
+    return sum;
+}
+void genebinary(int x[],int k,int n,int a[],int& m){
+    
+    if(k==n){
+        int temp = check(x,k,a);
+        m = max(temp,m);
+    }
+    else{
+        a[k] = 1;
+        genebinary(x,k+1,n,a,m);
+        a[k] = 0;
+        genebinary(x,k+1,n,a,m);
+    }
+}
+int main()
+{
+    int n = 10;
+    int arr[n] = {30,10,8,20,11,12,25,13,20,19};
+    int temp[n];
+    int Max = 0;
+    genebinary(arr, 0, n,temp,Max);
+    cout<<Max;
+}
