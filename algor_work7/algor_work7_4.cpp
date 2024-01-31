514b0886abe28d89a09ee22220bd3f77954f8b98
8ReTid8 <125667954+8ReTid8@users.noreply.github.com>
2024-01-31 23:15:55 +0700

    yo

---
+++ algor_work7/algor_work7_4.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int x[], int k,int& count)
+{
+    if(k==0){
+        cout<<0<<endl;
+        count++;
+    }
+    else{
+        for (int i = 1; i <= k; i++)
+        {
+            cout << x[i] << " ";
+        }
+        count++;
+        cout << endl;
+    }
+}
+void sub(int x[], int k, int n,int& count)
+{
+    print(x, k,count);
+    int j;
+    if(k == 0){
+        j = 1;
+    }
+    else{
+        j = x[k]+1;
+    }
+
+    for (int i = j; i <= n; i++)
+    {
+        x[k + 1] = i;
+        sub(x, k + 1, n,count);
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n] = {0};
+    int count = 0;
+    sub(arr,0,n,count);
+    cout<<count;
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void wow(int x[], int k)
 {
-    for (int i = 0; i < k; i++)
+    for (int i = 1; i <= k; i++)
     {
         cout << x[i] << " ";
     }
@@ -25,16 +25,16 @@ int check(int x[], int k,int a[])
     return sum;
 }
 
-// void sub(int x[], int k, int n)
-// {
-//     wow(x, k);
-//     int j = (k == 0) ? 1 : x[k] + 1;
-//     for (int i = j; i <= n; i++)
-//     {
-//         x[k + 1] = i;
-//         sub(x, k + 1, n);
-//     }
-// }
+void sub(int x[], int k, int n)
+{
+    wow(x, k);
+    int j = (k == 0) ? 1 : x[k] + 1;
+    for (int i = j; i <= n; i++)
+    {
+        x[k + 1] = i;
+        sub(x, k + 1, n);
+    }
+}
 void permui(int x[], int k, int n)
 {
     if (k == n)
@@ -75,7 +75,7 @@ int main()
     int temp[n];
     int Max = 0;
     // no5(arr, 0, n,temp,Max);
-    permui(arr, 0, n);
+    sub(arr, 0, n);
     // cout<<Max;
 }
 
