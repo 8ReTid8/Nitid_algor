df4ee240350b05dd5281a20948785e4fde5171fd
8ReTid8 <125667954+8ReTid8@users.noreply.github.com>
2024-02-03 16:33:25 +0700

    g

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,18 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> countsort(vector<int> arr){
+    int n = arr.size();
+    int m = 0;
+    int Max=0,Min=0;
+    for(int i=0;i<n;i++){
+        Max = max(Max,arr[i]);
+        Min = min(Min,arr[i]);
+    }
+    vector<int> countarr(Max-Min+1);
+    for(int i=0;i<n;i++){
+        countarr[arr[i]-Min]++;
+    }
+    for(int i=1;i<Max-Min+1;i++){
+        countarr[i]+=countarr[i-1];
+    }
+    vector<int> sortarr(n);
+    for(int i=0;i<n;i++){
+        countarr[arr[i]-Min]--;
+        sortarr[countarr[arr[i]-Min]] = arr[i];
+    }
+    return sortarr;
+}
 int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    string str[] = {"11","22","33"};
+    vector<int> arr(3);
+    for(int i=0;i<3;i++){
+        int sum = 0;
+        for(int j=0;j<str[i].length();j++){
+            sum += str[i][j]-'0';
+        }
+        arr[i] = sum;
     }
-    int sum = 0;
-    int ans = INT_MIN;
-    for(int i=0;i<n;i++){
-        sum = max(arr[i],sum+arr[i]);
-        ans = max(sum,ans);
+    
+    arr = countsort(arr);
+    for(int i=0;i<3;i++){
+        cout<<arr[i]<<" ";
     }
-    cout<<ans;
-    return 0;
 }
