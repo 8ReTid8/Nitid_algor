8fd32a65383ca42eb28ffffac6c0a31b48a2b90d
8ReTid8 <125667954+8ReTid8@users.noreply.github.com>
2024-03-11 12:16:17 +0700

    yeye

--- INCLASS/inclassdynamic.cpp
+++ INCLASS/inclassdynamic.cpp
@@ -84,57 +84,57 @@ int knap(int n,int k,bag arr[],int count){
 
 int gay[] = {2,3,5,6,8};
 vector<int> temp(100,0);
-// int c(int n){
-//     if(n==0){
-//         return ;
-//     }
-//     else if(k==0){
-//         return 1;
-//     }
-//     else{
-//         return c(n-1)*(n-1)+();
-//     }
-// }
-// int main()
-// {
-    // // cout << g(7) << f(6, 4);
-    // vector<int> g(100, -1);
-    // g[0] = 0;
-    // g[1] = 1;
-    // g[2] = 1;
-    // for (int i = 3; i <= 7; i++)
-    // {
-    //     g[i] = g[i - 1] + g[i - 2] - g[i - 3];
-    // }
-    // cout << g[7];
+int c(int n){
+    if(n==0){
+        return ;
+    }
+    else if(k==0){
+        return 1;
+    }
+    else{
+        return c(n-1)*(n-1)+();
+    }
+}
+int main()
+{
+    // cout << g(7) << f(6, 4);
+    vector<int> g(100, -1);
+    g[0] = 0;
+    g[1] = 1;
+    g[2] = 1;
+    for (int i = 3; i <= 7; i++)
+    {
+        g[i] = g[i - 1] + g[i - 2] - g[i - 3];
+    }
+    cout << g[7];
 
-    // vector<vector<int>> fi(100, vector<int>(100));
-    // // int fi[100][100];
-    // for (int i = 0; i <= 6; i++)
-    // {
-    //     for (int j = 0; j <= 4; j++)
-    //     {
-    //         if (i == j || j == 0 || j == 1)
-    //         {
-    //             fi[i][j] = 1;
-    //         }
-    //         else
-    //         {
-    //             fi[i][j] = fi[i - 1][j] + fi[i - 1][j - 2];
-    //         }
-    //     }
-    // }
-    // cout<<fi[6][4];
+    vector<vector<int>> fi(100, vector<int>(100));
+    // int fi[100][100];
+    for (int i = 0; i <= 6; i++)
+    {
+        for (int j = 0; j <= 4; j++)
+        {
+            if (i == j || j == 0 || j == 1)
+            {
+                fi[i][j] = 1;
+            }
+            else
+            {
+                fi[i][j] = fi[i - 1][j] + fi[i - 1][j - 2];
+            }
+        }
+    }
+    cout<<fi[6][4];
 
-//     int n = 4;
-//     int c = 7;
-//     bag arr[n];
-//     for(int i=0;i<n;i++){
-//         cin>>arr[i].v>>arr[i].w;
-//     }
-//     cout<<knap(n,c,arr,0);
-//     return 0;
-// }
+    int n = 4;
+    int c = 7;
+    bag arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i].v>>arr[i].w;
+    }
+    cout<<knap(n,c,arr,0);
+    return 0;
+}
 
 int main(){
     int n = 5;
