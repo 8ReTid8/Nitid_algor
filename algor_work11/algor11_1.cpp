1ec7c014514d28e5e40d3f49c071506c0905acd0
8ReTid8 <125667954+8ReTid8@users.noreply.github.com>
2024-03-27 15:12:55 +0700

    ั

--- algor_work11/algor11_1.cpp
+++ algor_work11/algor11_1.cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-// vector<vector<int>> temp(100,vector<int>(100,-1));
+vector<vector<int>> temp(100,vector<int>(100,-1));
 int dynamic(int n,int k,int arr[],int count){
     if(n==0&&k>0){
-        return 0;
+        return -999;
     }
     else if(k==0){
-        return count;
-    }
-    else if(k<0){
         return 0;
     }
-    // else if(temp[n][k]!=-1){
-    //     return temp[n][k];
-    // }
+    else if(k<0){
+        return -999;
+    }
+    else if(temp[n][k]!=-1){
+        return temp[n][k];
+    }
     else{
-        // temp[n][k] = max(dynamic(n-1,k,arr,count),dynamic(n-1,k-arr[n-1],arr,count+1));
-        // return temp[n][k];
-        return max(dynamic(n-1,k,arr,count),dynamic(n-1,k-arr[n-1],arr,count+1));
+        temp[n][k] = max(dynamic(n-1,k,arr,count),1+dynamic(n-1,k-arr[n-1],arr,count+1));
+        return temp[n][k];
+        // return max(dynamic(n-1,k,arr,count),dynamic(n-1,k-arr[n-1],arr,count+1));
     }
 }
 int main()
